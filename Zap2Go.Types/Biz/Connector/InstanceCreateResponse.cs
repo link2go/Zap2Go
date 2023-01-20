ec098437719e48c30335b6c4d94a777bd016a14b
Rafael Matos <rmatos@awg-ti.com.br>
2023-01-20 17:17:16 -0300

    criacao de instancia assincrona

--- Zap2Go.Types/Biz/Connector/InstanceCreateResponse.cs
+++ Zap2Go.Types/Biz/Connector/InstanceCreateResponse.cs
@@ -11,6 +11,12 @@ namespace Zap2Go.Types.Biz.Connector
         public string id { get; set; }
         public string token { get; set; }
 
+        public enum EnumStatusCreateInstance { SUCCESS = 0, ERROR = 1, CREATING = 2}
+
+        public EnumStatusCreateInstance status { get; set; } = EnumStatusCreateInstance.SUCCESS;
+
+        public string statusMessage { get; set; }
+
     }
 
 
